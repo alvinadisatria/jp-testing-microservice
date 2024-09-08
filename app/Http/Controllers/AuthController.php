bb2bf37ac7f84be07d190ef08d1154e03b4de8a7
alvinadisatria <aadisatria8@gmail.com>
2024-09-08 20:23:16 +0700

    jwt add

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,16 +27,27 @@ class AuthController extends Controller
         return response()->json(['access_token' => $token, 'token_type' => 'Bearer']);
     }
 
-    public function login(Request $request)
-    {
-        if (!Auth::attempt($request->only('email', 'password'))) {
-            return response()->json(['message' => 'Unauthorized'], 401);
+    // public function login(Request $request)
+    // {
+    //     if (!Auth::attempt($request->only('email', 'password'))) {
+    //         return response()->json(['message' => 'Unauthorized'], 401);
+    //     }
+
+    //     $user = User::where('email', $request->email)->firstOrFail();
+
+    //     $token = $user->createToken('auth_token')->plainTextToken;
+
+    //     return response()->json(['access_token' => $token, 'token_type' => 'Bearer']);
+    // }
+    public function login(Request $request) {
+        $credentials = $request->only('email', 'password');
+        
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
         }
-
-        $user = User::where('email', $request->email)->firstOrFail();
-
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-        return response()->json(['access_token' => $token, 'token_type' => 'Bearer']);
+    
+        return response()->json(['message' => 'Login successful'])
+            ->cookie('access_token', $token, 60, '/', null, true, true); // HTTP-only and Secure cookie
     }
+    
 }
