d68eba60c1d4c0f39ce7e2d31b4bfbaec65911e2
alvinadisatria <aadisatria8@gmail.com>
2024-09-15 19:20:48 +0700

    fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -161,7 +161,7 @@
                             </div>
                         </div>
                     </main> --}}
-
+                    <h1 class="text-center px-5 mx-5 text-white">Auth API GW</h1>
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </footer>
